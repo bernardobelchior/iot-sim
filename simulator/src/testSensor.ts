156b3659199b7be22f72478fb8d32ff8dea578d1
Bernardo Belchior <bernardo.belchior1@gmail.com>
2019-02-22 18:55:18 +0100

    Finish DeviceRegistry.

--- simulator/src/testSensor.ts
+++ simulator/src/testSensor.ts
@@ -1,29 +1,47 @@
 import { messageQueueBuilder } from "./api/MessageQueue";
 import { vars } from "./util/vars";
 
-const thing = {
-  name: "On/Off Switch",
-  description: "A web connected switch",
-  properties: {
-    on: {
-      title: "On/Off",
-      type: "boolean",
-      description: "Whether the lamp is turned on",
-      links: [{ href: "/things/lamp/properties/on" }]
-    }
+const things = [
+  {
+    name: "Lamp",
+    description: "A web connected lamp",
+    properties: {
+      on: {
+        title: "On/Off",
+        type: "boolean",
+        description: "Whether the lamp is turned on",
+        links: [{ href: "/things/lamp/properties/on" }]
+      }
+    },
+    href: "/things/lamp"
   },
-  href: "/things/lamp"
-};
+  {
+    name: "Window",
+    description: "A web connected window",
+    properties: {
+      open: {
+        title: "Open/Closed",
+        type: "boolean",
+        description: "Whether the window is open or closed",
+        links: [{ href: "/things/window/properties/open" }]
+      }
+    },
+    href: "/things/window"
+  }
+];
 
 async function run() {
   const messageQueue = await messageQueueBuilder(vars.AMQP_URI);
 
   await messageQueue.init();
   await messageQueue.createExchange("registry", "direct");
-  messageQueue.publish(
-    "registry",
-    "register",
-    Buffer.from(JSON.stringify(thing))
+
+  things.forEach(thing =>
+    messageQueue.publish(
+      "registry",
+      "register",
+      Buffer.from(JSON.stringify(thing))
+    )
   );
 }
 
